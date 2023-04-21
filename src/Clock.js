42f166ecf6be48d39474c6c7a6bcdffcb11b1ae0
william arasirwa <mageboarasirwa03@gmail.com>
2023-04-21 15:45:16 +0300

    complete

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,16 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalID = setInterval(() => {
+      console.log("running");
       setTime(new Date());
     }, 1000);
+
+    // clean up
+
+    return function () {
+      clearInterval(intervalID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
